699dd40ec329b05f7ca20570d3990eeb439e84af
borisovboris <boris.s.borisov99@gmail.com>
2024-01-06 23:16:53 +0300

    Add Angular Material to project

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTabsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'front-end';
+  title = 'signals-processing';
 }
