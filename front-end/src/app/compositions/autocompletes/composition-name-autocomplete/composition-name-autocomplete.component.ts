96e1d81ca5ab9fd382483e9e4772eaa64c7286d9
borisovboris <boris.s.borisov99@gmail.com>
2024-02-06 14:19:59 +0300

    Handle variety of values in composition name autocomplete

--- front-end/src/app/compositions/autocompletes/composition-name-autocomplete/composition-name-autocomplete.component.ts
+++ front-end/src/app/compositions/autocompletes/composition-name-autocomplete/composition-name-autocomplete.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, DestroyRef, EventEmitter, Input, Output, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { MaterialModule } from '../../../material/material.module';
 import { Observable, debounceTime, filter, of, tap } from 'rxjs';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { isDefined } from '../../../shared/utils';
 import { LabeledValue } from '../../../composition-details/link-composition-form/link-composition-form.component';
+import { MatOption } from '@angular/material/core';
 
 @Component({
   selector: 'app-composition-name-autocomplete',
@@ -14,39 +24,64 @@ import { LabeledValue } from '../../../composition-details/link-composition-form
   imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
   templateUrl: './composition-name-autocomplete.component.html',
   styleUrl: './composition-name-autocomplete.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CompositionNameAutocompleteComponent {
   private destroyRef = inject(DestroyRef);
-  nameCtrl = new FormControl('');
-  
-  @Input() 
+  nameCtrl: FormControl<LabeledValue<number> | string | null> = new FormControl(
+    ''
+  );
+  currentlySelectedOption!: MatOption<LabeledValue<number>>;
+
+  @Input()
   options$!: Observable<LabeledValue<number>[]>;
   @Output()
-  optionChanged = new EventEmitter<number | undefined>;
-  @Output() 
+  optionChanged = new EventEmitter<number | undefined>();
+  @Output()
   userTextInput = new EventEmitter<string>();
+  @ViewChild("auto") autocomplete!: MatAutocomplete;
 
   ngOnInit() {
     this.userTextInput.emit('');
 
+    // valueChanges is triggered when the user types in and
+    // also if an option from the autocomplete is selected.
+    // The value of this field can be an object or a simple string.
     this.nameCtrl.valueChanges
       .pipe(
-        // Remove option whenever the user types in
-        tap(() => this.optionChanged.emit(undefined)),
+        tap((value) => {
+          this.optionChanged.emit(undefined);
+          
+          // If an option was previously selected and the dropdown
+          // is no longer open. Clear option on first text input removal.
+          if(!this.autocomplete.isOpen) {
+            this.clearOption();
+          }
+        }),
         debounceTime(300),
-        filter((input) => isDefined(input) && input.length > 0),
+        filter((input) => isDefined(input)),
         takeUntilDestroyed(this.destroyRef)
       )
-      .subscribe(text => this.userTextInput.emit(text!));
+      .subscribe((value) => {
+        if (typeof value === 'string') {
+          this.userTextInput.emit(value);
+        }
+      });
   }
 
   selectOption(event: MatAutocompleteSelectedEvent) {
+    this.currentlySelectedOption = event.option;
     const labeledValue: LabeledValue<number> = event.option.value;
     this.optionChanged.emit(labeledValue.value);
   }
 
   displayFn(data: LabeledValue<number>): string {
     return data.label;
+  }
+
+  clearOption() {
+    if (this.currentlySelectedOption) {
+      this.currentlySelectedOption.deselect();
+    }
   }
 }
